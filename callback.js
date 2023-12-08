f5100ecffa0b5e728c63421287ff3bc508e95387
Asroyx <mystoganza@hotmail.com>
2023-12-08 23:55:26 +0300

    Add function sayHello

--- callback.js
+++ callback.js
@@ -1,5 +1,11 @@
 // setTimeout(() => console.log("Hello"), 5000);
 
-setInterval(() => {
-  console.log("Hello, I will work every second");
-}, 1000);
+// setInterval(() => {
+//   console.log("Hello, I will work every second");
+// }, 1000);
+
+const sayHello = () => {
+  console.log("Hello");
+};
+
+sayHello();
