0e827aec092a7c9f6eb953db65ee77fd58d2494f
Asroyx <mystoganza@hotmail.com>
2023-12-09 23:39:45 +0300

    Add fetch call to retrieve data

--- callback.js
+++ callback.js
@@ -4,8 +4,14 @@
 //   console.log("Hello, I will work every second");
 // }, 1000);
 
-const sayHello = (cb) => {
-  cb();
-};
+// const sayHello = (cb) => {
+//   cb();
+// };
 
-sayHello(() => console.log("Hello"));
+// sayHello(() => console.log("Hello"));
+
+import fetch from "node-fetch";
+
+fetch("https://jsonplaceholder.typicode.com/users").then((data) =>
+  console.log(data)
+);
