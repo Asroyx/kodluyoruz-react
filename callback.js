6e094cf531c9cc6bd78f56b265af0697a11ff045
Asroyx <mystoganza@hotmail.com>
2023-12-09 23:51:55 +0300

    Refactor code to load and log users, then load and log posts using fetch

--- callback.js
+++ callback.js
@@ -14,4 +14,10 @@ import fetch from "node-fetch";
 
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((data) => data.json())
-  .then((users) => console.log(users));
+  .then((users) => {
+    console.log("USERS LOADED", users);
+
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then((data) => data.json())
+      .then((posts) => console.log("POSTS LOADED", posts));
+  });
