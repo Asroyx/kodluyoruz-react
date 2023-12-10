4114865de4edf7d6a4e8daf42f9d7c862aeeb7ad
Asroyx <mystoganza@hotmail.com>
2023-12-10 21:03:11 +0300

    Refactor async function getData to sequentially fetch and log users and post1

--- callback.js
+++ callback.js
@@ -32,7 +32,12 @@ async function getData() {
   const users = await (
     await fetch("https://jsonplaceholder.typicode.com/users")
   ).json();
+  const post1 = await (
+    await fetch("https://jsonplaceholder.typicode.com/posts/1")
+  ).json();
+
   console.log("users", users);
+  console.log("post1", post1);
 }
 
 getData();
