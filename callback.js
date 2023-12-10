8eafc3942a7ab6923874a991c032aa94480901be
Asroyx <mystoganza@hotmail.com>
2023-12-10 20:57:20 +0300

    Add async function getData to fetch and log data

--- callback.js
+++ callback.js
@@ -12,18 +12,25 @@
 
 import fetch from "node-fetch";
 
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then((data) => data.json())
-  .then((users) => {
-    console.log("USERS LOADED", users);
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((data) => data.json())
+//   .then((users) => {
+//     console.log("USERS LOADED", users);
 
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-      .then((data) => data.json())
-      .then((posts) => {
-        console.log("POSTS LOADED", posts);
+//     fetch("https://jsonplaceholder.typicode.com/posts/1")
+//       .then((data) => data.json())
+//       .then((posts) => {
+//         console.log("POSTS LOADED", posts);
 
-        fetch("https://jsonplaceholder.typicode.com/posts/2")
-          .then((data) => data.json())
-          .then((posts2) => console.log("POSTS 2 LOADED", posts));
-      });
-  });
+//         fetch("https://jsonplaceholder.typicode.com/posts/2")
+//           .then((data) => data.json())
+//           .then((posts2) => console.log("POSTS 2 LOADED", posts));
+//       });
+//   });
+
+async function getData() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users");
+  console.log(data);
+}
+
+getData();
