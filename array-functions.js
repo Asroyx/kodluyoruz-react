981d0594fda1071b8954d041b53a974fb970483a
Asroyx <mystoganza@hotmail.com>
2023-12-19 23:54:42 +0300

    find search for user 'Mehmet' in users array

--- array-functions.js
+++ array-functions.js
@@ -1,3 +1,5 @@
+import { user } from "./my-module";
+
 const users = [{ name: "Mehnet" }, { name: "Murat" }, { name: "Sevilay" }];
 
 // push
@@ -7,6 +9,10 @@ const users = [{ name: "Mehnet" }, { name: "Murat" }, { name: "Sevilay" }];
 // console.log(users);
 
 // map
-users.map((item) => {
-  console.log(item.name);
-});
+// users.map((item) => {
+//   console.log(item.name);
+// });
+
+// find
+const result = users.find((item) => item.name === "Mehmet");
+console.log(result);
