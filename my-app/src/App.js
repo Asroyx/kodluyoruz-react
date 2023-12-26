381f9d1fe9819939623e4213c85110ddfdfc10a7
Asroyx <mystoganza@hotmail.com>
2023-12-26 23:33:27 +0300

    Use direct React.createElement call for 'Hello' text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./components/Header";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello React</h1>
-      <Header />
-    </div>
-  );
+  return React.createElement("div", null, "Hello");
+
+  // return (
+  //   <div>
+  //     <h1>Hello React</h1>
+  //     <Header />
+  //   </div>
+  // );
 }
 
 export default App;
