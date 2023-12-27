453d5922b85ebfd728ee28b3eb6beb739f2a213e
Asroyx <mystoganza@hotmail.com>
2023-12-27 23:49:24 +0300

    Add React import for component usage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,11 @@
 import "./App.css";
+import React from "react";
 
 import Header from "./components/Header";
 
 function App() {
   return React.createElement("div", null, "Hello");
-
-  // return (
-  //   <div>
-  //     <h1>Hello React</h1>
-  //     <Header />
-  //   </div>
-  // );
+  return <div></div>;
 }
 
 export default App;
