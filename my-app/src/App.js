8d68111adb59033933d1c4f0f9ff881a81e89854
Asroyx <mystoganza@hotmail.com>
2023-12-28 23:54:34 +0300

    Concatenate name and surname in the App component's heading

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,15 @@ import "./App.css";
 import Header from "./components/Header";
 
 const name = "Asil";
+const surname = "Ayazoglu";
 
 function App() {
   return (
     <>
-      <h1>{name}</h1>
+      <h1>
+        {name}
+        {surname}
+      </h1>
     </>
   );
 }
