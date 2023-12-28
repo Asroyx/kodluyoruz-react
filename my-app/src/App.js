d410c6dbbaf8b3d2e185701fc973a271294eae7a
Asroyx <mystoganza@hotmail.com>
2023-12-28 23:53:38 +0300

    Display the name 'Asil' in the App component's heading

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,20 +2,12 @@ import "./App.css";
 
 import Header from "./components/Header";
 
+const name = "Asil";
+
 function App() {
   return (
     <>
-      <Header />
-      <p className="xyz">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste vitae ad
-        odio sequi soluta commodi velit hic delectus molestiae ducimus quidem
-        voluptate veniam, sit numquam rem reprehenderit ullam minus amet.
-      </p>
-
-      <label htmlFor="myinput">
-        Name
-        <input id="myinput" />
-      </label>
+      <h1>{name}</h1>
     </>
   );
 }
