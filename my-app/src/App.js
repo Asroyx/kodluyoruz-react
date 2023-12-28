6173b6ebafcf0d4edf1b0ce44074f195d5a4cae6
Asroyx <mystoganza@hotmail.com>
2023-12-28 23:58:36 +0300

    Implement conditional rendering in App component based on user authentication status

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,12 @@ import Header from "./components/Header";
 
 const name = "Asil";
 const surname = "Ayazoglu";
-const isLoggedIn = true;
+const isLoggedIn = false; // change this to see information about name and surname
 
 function App() {
   return (
     <>
-      <h1>{`My name is ${name} and my surname is ${surname}`}</h1>
+      <h1>{isLoggedIn && `My name is ${name} and my surname is ${surname}`}</h1>
     </>
   );
 }
