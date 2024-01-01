93966abb864a4a2b60b92d6b803c103fb47456cb
Asroyx <mystoganza@hotmail.com>
2024-01-01 23:58:36 +0300

    Remove unnecessary variable declarations

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,6 @@ import "./App.css";
 
 import User from "./components/User";
 
-const name = "Asil";
-const surname = "Ayazoglu";
-const isLoggedIn = false; // change this to see information about name and surname
-
 function App() {
   return (
     <>
