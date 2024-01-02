78df8ea92330d98e360e7903d78c91f259eeeda0
Asroyx <mystoganza@hotmail.com>
2024-01-02 23:53:03 +0300

    Update User component usage with the new 'friends' prop

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,13 @@ import User from "./components/User";
 function App() {
   return (
     <>
-      <User name="Asil" surname="Ayazoglu" isLoggedIn={false} age={29} />
+      <User
+        name="Asil"
+        surname="Ayazoglu"
+        isLoggedIn={false}
+        age={29}
+        friends={["Ahmet", "Tayfun", "Gökhan", "Ayşe", "Fatma"]}
+      />
     </>
   );
 }
