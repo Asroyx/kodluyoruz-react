6bf09c593ac65f7936c813086f5d0ac80c8e5a49
Asroyx <mystoganza@hotmail.com>
2024-01-02 23:59:27 +0300

    Modify User component to include index in 'friends' list rendering

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -2,8 +2,10 @@ function User(name, surname, isLoggedIn, age, friends) {
   return;
   <>
     <h1>{isLoggedIn ? `${name} ${surname} ${age}` : "Not logged in."}</h1>;{" "}
-    {friends.map((friend) => (
-      <div>{friend}</div>
+    {friends.map((friend, index) => (
+      <div>
+        {index}- {friend}
+      </div>
     ))}
   </>;
 }
