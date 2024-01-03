40a3a23d5f543a62aa7dc96d7b1fea6e5689b530
Asroyx <mystoganza@hotmail.com>
2024-01-03 23:45:48 +0300

    Add 'key' prop to each friend element in User component

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -3,7 +3,7 @@ function User(name, surname, isLoggedIn, age, friends) {
   <>
     <h1>{isLoggedIn ? `${name} ${surname} ${age}` : "Not logged in."}</h1>;{" "}
     {friends.map((friend, index) => (
-      <div>
+      <div key={index}>
         {index}- {friend}
       </div>
     ))}
