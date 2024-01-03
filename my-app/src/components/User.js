6d5e95d8e1773e98965a3351bce349d8752e92d9
Asroyx <mystoganza@hotmail.com>
2024-01-03 23:48:04 +0300

    Fix syntax error by removing unnecessary component

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -1,7 +1,7 @@
 function User(name, surname, isLoggedIn, age, friends) {
   return;
   <>
-    <h1>{isLoggedIn ? `${name} ${surname} ${age}` : "Not logged in."}</h1>;{" "}
+    <h1>{isLoggedIn ? `${name} ${surname} ${age}` : "Not logged in."}</h1>;
     {friends.map((friend, index) => (
       <div key={index}>
         {index}- {friend}
