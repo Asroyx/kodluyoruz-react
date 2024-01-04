68ceffa0f5f722815d0d8de1655ca1926926d1fb
Asroyx <mystoganza@hotmail.com>
2024-01-04 23:54:32 +0300

    Fix syntax error

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -1,11 +1,12 @@
 function User(name, surname, isLoggedIn, age, friends) {
-  return;
-  <>
-    <h1>{isLoggedIn ? `${name} ${surname} ${age}` : "Not logged in."}</h1>;
-    {friends.map((friend, index) => (
-      <div key={friend.id}>{friend.name}</div>
-    ))}
-  </>;
+  return (
+    <>
+      <h1>{isLoggedIn ? `${name} ${surname} ${age}` : "Not logged in."}</h1>;
+      {friends.map((friend, index) => (
+        <div key={friend.id}>{friend.name}</div>
+      ))}
+    </>
+  );
 }
 
 export default User;
