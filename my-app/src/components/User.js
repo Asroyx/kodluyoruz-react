adfe7cb022c4bcbe1a0dee5573b8f8289b9b5abe
Asroyx <mystoganza@hotmail.com>
2024-01-07 23:20:41 +0300

    Refactor PropTypes in User component for better readability and maintainability

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -4,7 +4,7 @@ function User({ name, surname, isLoggedIn, age, friends }) {
   return (
     <>
       <h1>{isLoggedIn ? `${name} ${surname} ${age}` : "Not logged in."}</h1>;
-      {friends.map((friend, index) => (
+      {friends.map((friend) => (
         <div key={friend.id}>{friend.name}</div>
       ))}
     </>
